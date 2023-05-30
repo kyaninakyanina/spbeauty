0c47d1b5e7b5edfe749d641fa01db6112910aa3a
Yanina <131899863+kyaninakyanina@users.noreply.github.com>
2023-05-30 14:58:51 +0000

    Сделать новый фоновый цвет SerchField 2

--- src/Layouts/Page/Header/Components/SearchField.js
+++ src/Layouts/Page/Header/Components/SearchField.js
@@ -11,7 +11,7 @@ const Input = styled.input`
     outline: none;
     border-radius: 0px;
     border: solid #152842 1px;
-    background-color: #D9E2EF;
+    background-color: #F8FAFC;
     color: #152842;
     width: 100%;
     &:hover {
