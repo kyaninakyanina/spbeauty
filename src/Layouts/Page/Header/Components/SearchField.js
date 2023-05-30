3102797795a21cf978dc6ce97b1d6f0cd37bdafc
Yanina <131899863+kyaninakyanina@users.noreply.github.com>
2023-05-30 14:55:01 +0000

    Сделалf новый фоновый цвет SerchField

--- src/Layouts/Page/Header/Components/SearchField.js
+++ src/Layouts/Page/Header/Components/SearchField.js
@@ -23,7 +23,7 @@ const Input = styled.input`
 `;
 
 const SearchIconWrapper = styled.div`
-    background-color: #4B759E;
+    background-color: #F8FAFC;
     position: absolute;
     box-sizing:border-box;
     right: 0px;  /* Здесь можно добавить отступ от правого края, чтобы сдвинуть иконку левее */
