9bc4a8ae67702a7e4ffe3da731d544edd2dd6a9c
Yanina <131899863+kyaninakyanina@users.noreply.github.com>
2023-05-30 17:42:58 +0000

    Обновить стили шрифтов в typography.css

--- src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
@@ -13,8 +13,8 @@ function ListElement(props) {
     return (
         <div className='d-flex flex-row align-items-center align-self-stretch gap-2'>
             <Option className='flex-grow-1 p-2'>{props.children}</Option>
-            <SecondaryButton minWidth='0'>Ред.</SecondaryButton>
-            <SecondaryButton minWidth='0'>Удл.</SecondaryButton>
+            <SecondaryButton minWidth='0'>EditIcon</SecondaryButton>
+            <SecondaryButton minWidth='0'>RemoveIcon</SecondaryButton>
         </div>
     );
 }
