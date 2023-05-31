46050b8e192ec80e598448efb508206650d5eba4
Yanina <131899863+kyaninakyanina@users.noreply.github.com>
2023-05-31 15:42:10 +0000

    Подровнять паддинги у ListElement

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -4,7 +4,7 @@ import ListElement from "./ListElement";
 function Addresses() {
     return (
         <div className='d-inline-flex flex-column gap-2 mt-4 px-4 py-4 border-adresses'>
-            <div className='title'>Адреса заведений</div>
+            <div className='title ps-2'>Адреса заведений</div>
             <div className='d-inline-flex flex-column gap-3'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
